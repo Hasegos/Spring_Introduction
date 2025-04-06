5bd14e6bd22d0f28cc228106016e23954bb3bd98
최수호 <csw020106@naver.com>
2025-04-06 23:45:48 +0900

    사용자로부터 받은 이름 저장 클래스

---
+++ src/main/java/hello/hello_spring/controller/MemberForm.java
@@ -0,0 +1,15 @@
+package hello.hello_spring.controller;
+
+
+public class MemberForm {
+
+    private String Name;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+}
